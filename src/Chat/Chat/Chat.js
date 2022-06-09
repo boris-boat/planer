da6db43046b539c2118511e953a2796c44866a7c
boris-boat <84967370+boris-boat@users.noreply.github.com>
2022-06-09 16:18:19 +0200

    fixed some bugs

--- src/Chat/Chat/Chat.js
+++ src/Chat/Chat/Chat.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import "./Chat.css";
 import io from "socket.io-client";
-import { useLocation } from "react-router-dom";
+import {  useLocation, useNavigate } from "react-router-dom";
 import TextContainer from "../components/TextContainer/TextContainer";
 import Messages from "../components/Messages/Messages";
 import InfoBar from "../components/InfoBar/InfoBar";
 import Input from "../components/Input/Input";
+import { Button } from "react-bootstrap";
 let socket;
 
 const Chat = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   // const [name, setName] = useState("");
   const [room, setRoom] = useState("");
@@ -18,10 +20,8 @@ const Chat = () => {
   const [message, setMessage] = useState("");
   const ENDPOINT = "https://boris-chatapp.herokuapp.com/";
 
-  
   let name = localStorage.getItem("user")?.split(" ")[0];
   useEffect(() => {
-    
     const { room } = queryString.parse(location.search);
     setRoom(room);
     socket = io(ENDPOINT);
@@ -33,7 +33,7 @@ const Chat = () => {
     });
 
     return () => {
-      socket.emit("disconnect");
+      socket.disconnect()
       socket.off();
     };
   }, []);
@@ -57,17 +57,27 @@ const Chat = () => {
   };
 
   return (
-    <div className="outerContainer">
-      <div className="chatContainer">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
-      </div>
-      <TextContainer users={users} />
+    <div>
+      {name ? (
+        <div className="outerContainer">
+          <div className="chatContainer">
+            <InfoBar room={room} />
+            <Messages messages={messages} name={name} />
+            <Input
+              message={message}
+              setMessage={setMessage}
+              sendMessage={sendMessage}
+            />
+          </div>
+          <TextContainer users={users} />
+        </div>
+          
+      ) : ( <div className="App">
+      <h1 className="mt-5">Please create an account or login !</h1>
+      <Button variant="primary" onClick={() => navigate("/")}>
+        Back
+      </Button>
+    </div>)}
     </div>
   );
 };
--- src/Chat/components/Join/Join.js
+++ src/Chat/components/Join/Join.js
@@ -1,25 +1,36 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-import './Join.css';
+import "./Join.css";
 
 export default function SignIn() {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
+  const navigate = useNavigate();
+
+  const [room, setRoom] = useState("");
   let user = localStorage.getItem("user")?.split(" ")[0];
-  console.log(name)
+
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
-        
+
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => {
-            setName(user)
-            setRoom(event.target.value)}} />
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
         </div>
-        <Link onClick={e => (!room) ? e.preventDefault() : null} to={`/chat?room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
+        <Link
+          onClick={(e) => (!room ? e.preventDefault() : null)}
+          to={`/chat?room=${room}`}
+        >
+          <button className={"button mt-20"} type="submit">
+            Sign In
+          </button>
         </Link>
       </div>
     </div>

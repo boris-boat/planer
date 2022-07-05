00ab062e6f267956b78a99f208786484c8659512
boris-boat <84967370+boris-boat@users.noreply.github.com>
2022-07-05 17:21:57 +0200

    reworked to do loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,21 @@
 import { Container, Col } from "react-bootstrap";
 import "../App.css";
-
-
+import { ReactComponent as ToDoImg } from "../components/media/ilustrations/to_do_list.svg";
 import { useNavigate } from "react-router-dom";
 
 import { useEffect } from "react";
 
 const Home = () => {
-
   const navigate = useNavigate();
 
   let user = localStorage?.getItem("user")?.split(" ")[0];
 
   // let token = localStorage.getItem("token");
- 
- 
-  const textStyle = {verticalAlign : "middle",marginBottom : "0"}
+
+  const textStyle = { verticalAlign: "middle", marginBottom: "0" };
   const center =
     "d-flex align-content-center justify-content-center align-items-center";
   useEffect(() => {
-   
-    
     navigator.geolocation.getCurrentPosition(function (position) {
       localStorage.setItem("long", position.coords.longitude);
       localStorage.setItem("lat", position.coords.latitude);
@@ -32,71 +27,71 @@ const Home = () => {
   return (
     <>
       <Container className={center} style={{ height: "100vh" }}>
-     
-          <>
-            <Col style={{ paddingTop: "100px", height: "100vh" }}>
-              <Col className={`mb-5 ${center} text-center`}>
-                <h1 style={{ color: "white" }}>Greetings {user}</h1>
-              </Col>
-
-              <Col className={`${center}`}>
-                <Container style={{ width: "60%" }} className="homeContainer">
-                  {" "}
-                  <div className="home" onClick={() => navigate("/todo")}>
-                    <h4 style={textStyle}>ToDo's / Reminders</h4>{" "}
-                    <img
-                      className="logo"
-                      src={require("../components/media/Icons/todo.jpg")}
-                      alt=""
-                    ></img>
-                  </div>
-                  <div className="home" onClick={() => navigate("/torrent")}>
-                    {" "}
-                    <h4 style={textStyle}>Torrent tracker</h4>
-                    <img
-                      className="logo"
-                      src={require("../components/media/Icons/torrent.png")}
-                      alt=""
-                    ></img>
-                  </div>
-                  <div className="home" onClick={() => navigate("/news")}>
-                    <h4 style={textStyle}>Daily news</h4>{" "}
-                    <img
-                      className="logo"
-                      src={require("../components/media/Icons/news.png")}
-                      alt=""
-                    ></img>{" "}
-                  </div>
-                  <div className="home" onClick={() => navigate("/tracker")}>
-                    <h4 style={textStyle}>Expense tracker </h4>
-                    <img
-                      className="logo align-self-center"
-                      src={require("../components/media/Icons/money.png")}
-                      alt=""
-                    ></img>{" "}
-                  </div>
-                  <div className="home " onClick={() => navigate("/cookbook")}>
-                    <h4 style={textStyle}>Kitchen Assistant</h4>
-                    <img
-                      className="logo align-self-center"
-                      src={require("../components/media/Icons/hat.png")}
-                      alt=""
-                    ></img>
-                  </div>
-                  <div className="home " onClick={() => navigate("/quiz")}>
-                    <h4 style={textStyle}>Quiz</h4>
-                    <img
-                      className="logo align-self-center"
-                      src={require("../components/media/Icons/quiz.png")}
-                      alt=""
-                    ></img>
-                  </div>
-                 
-                </Container>
-              </Col>
+        <>
+          <Col style={{ paddingTop: "100px", height: "100vh" }}>
+            <Col className={`mb-5 ${center} text-center`}>
+              <h1 style={{ color: "white" }}>Greetings {user}</h1>
             </Col>
-          </>
-      
+
+            <Col className={`${center}`}>
+              <Container style={{ width: "60%" }} className="homeContainer">
+                {" "}
+                <div className="home toDo" onClick={() => navigate("/todo")}>
+                  <h4 style={textStyle}>ToDo's / Reminders</h4>{" "}
+                  <img
+                    className="logo"
+                    src={require("../components/media/Icons/todo.jpg")}
+                    alt=""
+                  ></img>
+                </div>
+                <div className="logo align-self-center toDoListImg">
+                  <ToDoImg />
+                </div>
+                <div className="home" onClick={() => navigate("/torrent")}>
+                  {" "}
+                  <h4 style={textStyle}>Torrent tracker</h4>
+                  <img
+                    className="logo"
+                    src={require("../components/media/Icons/torrent.png")}
+                    alt=""
+                  ></img>
+                </div>
+                <div className="home" onClick={() => navigate("/news")}>
+                  <h4 style={textStyle}>Daily news</h4>{" "}
+                  <img
+                    className="logo"
+                    src={require("../components/media/Icons/news.png")}
+                    alt=""
+                  ></img>{" "}
+                </div>
+                <div className="home" onClick={() => navigate("/tracker")}>
+                  <h4 style={textStyle}>Expense tracker </h4>
+                  <img
+                    className="logo align-self-center"
+                    src={require("../components/media/Icons/money.png")}
+                    alt=""
+                  ></img>{" "}
+                </div>
+                <div className="home " onClick={() => navigate("/cookbook")}>
+                  <h4 style={textStyle}>Kitchen Assistant</h4>
+                  <img
+                    className="logo align-self-center"
+                    src={require("../components/media/Icons/hat.png")}
+                    alt=""
+                  ></img>
+                </div>
+                <div className="home " onClick={() => navigate("/quiz")}>
+                  <h4 style={textStyle}>Quiz</h4>
+                  <img
+                    className="logo align-self-center"
+                    src={require("../components/media/Icons/quiz.png")}
+                    alt=""
+                  ></img>
+                </div>
+              </Container>
+            </Col>
+          </Col>
+        </>
       </Container>
     </>
   );
--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -10,7 +10,6 @@ import {
   Spinner,
 } from "react-bootstrap";
 
-
 import Quote from "../components/Quote";
 import Vreme from "../components/vreme";
 import { useStateContext } from "../components/StateContext";
@@ -67,17 +66,16 @@ function ToDo() {
     );
   };
 
- 
   useEffect(() => {
-  
     const getTodos = async () => {
       fetch(REACT_APP_API_URL + "/todos/" + user)
         .then((res) => res.json())
         .then((result) => setTodos(result))
         .catch((e) => console.log("Database error  : " + e));
+      setLoading(false);
     };
     getTodos();
-    setLoading(false);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const {
@@ -86,62 +84,61 @@ function ToDo() {
     VremeShow,
     setVremeShow,
 
-
     newTodo,
     setnewTodo,
   } = useStateContext();
   const center = "d-flex justify-content-center align-items-center";
   return (
     <div className="todoContainer">
-  
-        <>
-          <Row className="todoListContainer" style={{ margin: "0" }}>
-            <Vreme show={VremeShow} onHide={() => setVremeShow(false)} />
-            <Quote />
-            <Container style={{ height: "auto", padding: "0" }}>
-              <h1 className="mt-3" style={{ color: "white" }}>
-                Welcome {user} !
-              </h1>
+      <>
+        <Row className="todoListContainer" style={{ margin: "0" }}>
+          <Vreme show={VremeShow} onHide={() => setVremeShow(false)} />
+          <Quote />
+          <Container style={{ height: "auto", padding: "0" }}>
+            <h1 className="mt-3" style={{ color: "white" }}>
+              Welcome {user} !
+            </h1>
 
-              <Row className="d-inline-flex mt-3">
-                <Form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    setnewTodo("");
-                    addToDo();
-                  }}
-                >
-                  <InputGroup className="mb-3 flex-wrap-nowrap">
-                    <input
-                      className="input-field"
-                      placeholder="Add new item"
-                      value={newTodo}
-                      onChange={(e) => setnewTodo(e.target.value)}
-                    />
-                    <Button type="submit" variant="info" id="button-addon2">
-                      Add
-                    </Button>
-                  </InputGroup>
-                </Form>
-              </Row>
-            </Container>
-
-            <Container
-              className={`${center} flex-column`}
-              style={{ padding: "0" }}
-            >
-              <Row style={{ width: "100vw", margin: "0" }} className={center}>
-                <CategorySelector style={{ width: "100%" }} />
-              </Row>
-              <div className="mb-3" style={{ color: "white" }}>
-                Category : {category}
-              </div>
-
-              <ListGroup
-                className={`${center} listGrupaToDo`}
-                style={{ textAlign: "start", width: "50%" }}
+            <Row className="d-inline-flex mt-3">
+              <Form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setnewTodo("");
+                  addToDo();
+                }}
               >
-                {" "}
+                <InputGroup className="mb-3 flex-wrap-nowrap">
+                  <input
+                    className="input-field"
+                    placeholder="Add new item"
+                    value={newTodo}
+                    onChange={(e) => setnewTodo(e.target.value)}
+                  />
+                  <Button type="submit" variant="info" id="button-addon2">
+                    Add
+                  </Button>
+                </InputGroup>
+              </Form>
+            </Row>
+          </Container>
+
+          <Container
+            className={`${center} flex-column`}
+            style={{ padding: "0" }}
+          >
+            <Row style={{ width: "100vw", margin: "0" }} className={center}>
+              <CategorySelector style={{ width: "100%" }} />
+            </Row>
+            <div className="mb-3" style={{ color: "white" }}>
+              Category : {category}
+            </div>
+
+            <ListGroup
+              className={`${center} listGrupaToDo`}
+              style={{ textAlign: "start", width: "50%" }}
+            >
+              {" "}
+              {todos.length !== 0 && (
                 <div
                   style={{ width: "70%" }}
                   className="d-flex justify-content-center"
@@ -155,63 +152,68 @@ function ToDo() {
                     placeholder="Search ToDos"
                   ></input>
                 </div>
-                <div className="cela-grupa" style={{ borderRadius: "10px" }}>
-                  {loading && (
-                    <Spinner
-                      style={{ display: "flex", alignContent: "center" }}
-                      animation="border"
-                      role="status"
-                    ></Spinner>
-                  )}
-                  {todos.length !== 0 ? (
-                    todos
-                      .filter((val) => {
-                        if (search === "") {
-                          return val;
-                        } else if (
-                          val.text.toLowerCase().includes(search.toLowerCase())
-                        ) {
-                          return val;
-                        } else {
-                          return null;
-                        }
-                      })
-                      .map((todo) => {
-                        if (todo.category === category) {
-                          return (
-                            <ListGroup.Item key={todo._id}>
-                              <Item
-                                item={todo}
-                                completeTodo={completeTodo}
-                                deleteToDo={deleteToDo}
-                              />
-                            </ListGroup.Item>
-                          );
-                        } else if (category === "Everything") {
-                          return (
-                            <ListGroup.Item key={todo._id}>
-                              <Item
-                                item={todo}
-                                completeTodo={completeTodo}
-                                deleteToDo={deleteToDo}
-                              />
-                            </ListGroup.Item>
-                          );
-                        } else {
-                          return null;
-                        }
-                      })
-                  ) : (
-                    <div style={{ width: "auto",alignSelf : "center",textAlign : "center"}}>
-                      <h3 style={{ color: "white" }}>Please add some todos!</h3>
-                    </div>
-                  )}
-                </div>
-              </ListGroup>
-            </Container>
-          </Row>
-        </>
-     
+              )}
+              <div className="cela-grupa" style={{ borderRadius: "10px" }}>
+                {loading ? (
+                  <Spinner
+                    style={{ display: "flex", alignContent: "center" }}
+                    animation="border"
+                    role="status"
+                  ></Spinner>
+                ) : todos.length !== 0 ? (
+                  todos
+                    .filter((val) => {
+                      if (search === "") {
+                        return val;
+                      } else if (
+                        val.text.toLowerCase().includes(search.toLowerCase())
+                      ) {
+                        return val;
+                      } else {
+                        return null;
+                      }
+                    })
+                    .map((todo) => {
+                      if (todo.category === category) {
+                        return (
+                          <ListGroup.Item key={todo._id}>
+                            <Item
+                              item={todo}
+                              completeTodo={completeTodo}
+                              deleteToDo={deleteToDo}
+                            />
+                          </ListGroup.Item>
+                        );
+                      } else if (category === "Everything") {
+                        return (
+                          <ListGroup.Item key={todo._id}>
+                            <Item
+                              item={todo}
+                              completeTodo={completeTodo}
+                              deleteToDo={deleteToDo}
+                            />
+                          </ListGroup.Item>
+                        );
+                      } else {
+                        return null;
+                      }
+                    })
+                ) : (
+                  <div
+                    style={{
+                      width: "auto",
+                      alignSelf: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <h3 style={{ color: "white" }}>Please add some todos!</h3>
+                  </div>
+                )}
+              </div>
+            </ListGroup>
+          </Container>
+        </Row>
+      </>
     </div>
   );
 }

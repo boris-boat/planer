e727aeb68614097e8d3566b3804c2a8398e5cd6e
boris-boat <84967370+boris-boat@users.noreply.github.com>
2022-07-07 23:19:26 +0200

    switched to self hosting server

--- src/pages/LoginSignup/LoginSignup.jsx
+++ src/pages/LoginSignup/LoginSignup.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useStateContext } from "../../components/StateContext";
 import "../../App.css"
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
